a332c0a3d9494626c8564831bbd6c7e5add192f6
Malachi Schram <48308123+schr476@users.noreply.github.com>
2025-07-31 12:14:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup, find_packages
 
+def get_packages(filepath: str = './requirements.txt'):
+    with open(filepath, 'r') as file:
+       lines = file.readlines()
+    packages = []
+    for line in lines:
+       stripped_line = line.strip()
+       if (len(stripped_line) > 0) and (not stripped_line.startswith('#')):
+          packages.append(stripped_line)
+    return packages
+    
 setup(
     name="jlab_datascience_toolkit",
     version="0.1",
